0d84b6a0222c90f140cbff4702c1486e62832d15
xocliwtb <xocliwtb@users.noreply.github.com>
2015-04-20 00:32:51 -0400

    use memcpy, global variables for recalculate, save 3ms on recalculation

--- ArduinoConwaysGameOfLife.ino
+++ ArduinoConwaysGameOfLife.ino
@@ -1,16 +1,22 @@
 #include "U8glib.h"
 
-// Number of 8-bit words horizontal display.
+// Number of 8-bit bytes horizontal display.
 #define HH 16
 // Number of 1-pixel rows down on display.
 #define RR 64
 
 // U8glib documentation: https://code.google.com/p/u8glib/ 
-//U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_FAST);
+//U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE); // 215ms draw, 82ms recalculate
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_FAST); // 142ms draw, 82ms recalculate
 
 // one extra row at the bottom so we don't have to wrap
 uint8_t full_bitmap[(RR+1)*HH];
+
+// new row is stored only when row +1 is already re-calculated
+uint8_t new_top[HH];
+
+// this is the new row we are always working on
+uint8_t new_row[HH];
 
 //#define STATS_ON
 // stats
@@ -19,6 +25,12 @@ int cellsOn;
 int cellsChanged;
 int generation = 0;
 #endif
+
+//unsigned long time1;
+//unsigned long time2;
+
+// Recalculate Globals
+uint8_t Nrow, Srow, Wcol, Ecol, NWCell, NCell, NECell, WCell, XCell, ECell, SWCell, SCell, SECell, newXCell;
 
 // Seed with some objects
 void initialize2(void) {
@@ -131,25 +143,25 @@ void recalculate() {
   // copy "row #0" to "row #64" so we don't have to roll over at last row (array now has 65 rows)
   // save "row-1" original row for use in calculations
   // save "row" for use in next row calculations
-  uint8_t new_top[HH];      // new row is stored only when row +1 is already re-calculated
-  uint8_t new_row[HH];      // this is the new row we are always working on
   
   // Initialize row #64 to row#0 values
-  Serial.println(F("Copy Row0 to RowRR"));
-  for(int x=0;x<HH;x++) {
-    full_bitmap[HH*RR+x] = full_bitmap[x];
-  }
+//  Serial.println(F("Copy Row0 to RowRR"));
+//  for(int x=0;x<HH;x++) {
+//    full_bitmap[HH*RR+x] = full_bitmap[x];
+//  }
+
+  memcpy(&full_bitmap+HH*RR,&full_bitmap,HH);
 
   // Re-calculate each row
   for(int r=0;r<RR;r++) {
     
-    uint8_t Nrow = (r-1+RR)%RR;
-    uint8_t Srow = r+1; // no %RR, we copied row#0
+    Nrow = (r-1+RR)%RR;
+    Srow = r+1; // no %RR, we copied row#0
 
     // for each row, calculate one byte/column at a time
     for(int c=0;c<HH;c++) {
-      uint8_t Wcol = (c-1+HH)%HH;
-      uint8_t Ecol = (c+1)%HH;
+      Wcol = (c-1+HH)%HH;
+      Ecol = (c+1)%HH;
 
       // N neighbor  = byte# full_bitmap[((r-1)%64)*16+c] // r+1%64
       // S neighbor  = byte# full_bitmap[(r+1)*16+c+16] // do not use %64 on the last row, we copied row#0 so we don't need to roll over
@@ -161,19 +173,19 @@ void recalculate() {
       // SW neighbor = byte# full_bitmap[((r+1)%64)*16+((c-1)%16)]  // r+1%64, c-1%16
 
       // Top row
-      uint8_t NWCell = full_bitmap[Nrow*HH + Wcol];
-      uint8_t NCell  = full_bitmap[Nrow*HH + c];
-      uint8_t NECell = full_bitmap[Nrow*HH + Ecol];
+      NWCell = full_bitmap[Nrow*HH + Wcol];
+      NCell  = full_bitmap[Nrow*HH + c];
+      NECell = full_bitmap[Nrow*HH + Ecol];
 
       // Middle row
-      uint8_t WCell  = full_bitmap[r*HH + Wcol];
-      uint8_t XCell  = full_bitmap[r*HH + c];
-      uint8_t ECell  = full_bitmap[r*HH + Ecol];
+      WCell  = full_bitmap[r*HH + Wcol];
+      XCell  = full_bitmap[r*HH + c];
+      ECell  = full_bitmap[r*HH + Ecol];
 
       // Bottom row
-      uint8_t SWCell = full_bitmap[Srow*HH + Wcol];
-      uint8_t SCell  = full_bitmap[Srow*HH + c];
-      uint8_t SECell = full_bitmap[Srow*HH + Ecol];
+      SWCell = full_bitmap[Srow*HH + Wcol];
+      SCell  = full_bitmap[Srow*HH + c];
+      SECell = full_bitmap[Srow*HH + Ecol];
 
       // Start with the values themselves, shift left.  We have 8-bits surrounded by zeroes now
       unsigned int topRow = NCell << 1;
@@ -190,8 +202,9 @@ void recalculate() {
       bitWrite(midRow,0,bitRead(ECell,7));
       bitWrite(botRow,0,bitRead(SECell,7));
 
-      // We now have bit 0 from the east, bits 1-8 from the center and bit 9 from the west      
-      uint8_t newXCell = 0;
+      // We now have bit 0 from the east, bits 1-8 from the center and bit 9 from the west
+      // Initialize new byte with old byte values, we will only flip the changed bits
+//      newXCell = XCell; // this byte will be overwritten each time
 
       // Now start with bit 1 in the middle row and re-calculate bits 1-8
       for(int b=0;b<8;b++) {
@@ -207,7 +220,7 @@ void recalculate() {
                        +bitRead(botRow,1)
                        +bitRead(botRow,2);
 
-        // Calculate Next Generation        
+        // Calculate Next Generation
         if(neighbors==3 || (bitRead(midRow,1)==1 && neighbors==2)) {
 #ifdef STATS_ON
           if (bitRead(midRow,1) == 0)
@@ -234,27 +247,24 @@ void recalculate() {
 
     }
 
-    // save next gen top row back to matrix
-    for(int x=0;x<HH;x++) {
-      // Save the top row that we no longer need to use.  Skip for the very first row since we haven't pushed new_top to new_row yet
-      if(r>0) {
-        full_bitmap[HH*(r-1)+x] = new_top[x];
-      }
-      // bump new row into the top slot
-      new_top[x] = new_row[x];
+    // Save the top row that we no longer need to use.  Skip for the very first row since we haven't pushed new_top to new_row yet
+    if (r>0) {
+      memcpy(&full_bitmap[HH*(r-1)],&new_top,HH);
     }
+
+    // bump new row into the top slot
+    memcpy(&new_top,&new_row,HH);
+
   }
 
   // write the last row
-  for(int x=0;x<HH;x++) {
-    full_bitmap[(RR-1)*HH+x] = new_row[x];
-  }
+  memcpy(&full_bitmap+(RR-1)*HH,&new_row,HH);
   // row RR is ignored, it will be over-written next time we refresh
 
 }
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
 //  initialize(255); // random
   initialize2();
   // flip screen, if required
@@ -279,6 +289,8 @@ void setup(void) {
 }
 
 void loop(void) {
+//  time1 = millis();
+
   // picture loop
   u8g.firstPage();
   do {
@@ -288,10 +300,21 @@ void loop(void) {
     drawGeneration();
 #endif
   } while( u8g.nextPage() );
-  
+
+//  time2 = millis();
+
+//  Serial.print("Draw: ");
+//  Serial.print(time2-time1);
+
   // rebuild the picture after some delay
   recalculate();
 
-  //delay(10);
+//  time1 = millis();
+
+//  Serial.print(", Recalculate: ");
+//  Serial.print(time1-time2);
+//  Serial.print(": ");
+//  Serial.println(time1);
+
 }
 
